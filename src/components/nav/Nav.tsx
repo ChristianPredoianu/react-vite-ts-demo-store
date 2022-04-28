6e8291f50eff5e11be53ecbeaf5304aa78f97e8c
Christian Predoianu <christianpredoianu@yahoo.com>
2022-04-28 14:53:58 +0200

    work on nav

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,40 +1,44 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import NavLogo from '@/assets/fakestore2.png';
 import useScreenWidth from '@/hooks/useScreenWidth';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { CSSTransition } from 'react-transition-group';
+import NavLinks from '@/components/nav/NavLinks';
+import HamburgerIcon from '@/components/nav/HamburgerIcon';
 import classes from '@/components/nav/Nav.module.scss';
 
-export default function Nav(): JSX.Element {
+export default function Nav() {
   const [screenWidth] = useScreenWidth();
 
   const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const nodeRef = useRef<HTMLUListElement | null>(null);
 
   function toggleNavHandler() {
     setIsNavOpen(!isNavOpen);
   }
 
-  const hamburgerIcon = (
-    <FontAwesomeIcon
-      icon={faBars}
-      className={classes.Hamburger}
-      onClick={toggleNavHandler}
-    />
-  );
-
   return (
     <header className="container">
-      <nav className={classes.Nav}>
+      <nav className={classes.nav}>
         <img src={NavLogo} alt="Logo" />
-        {(isNavOpen || screenWidth > 640) && (
-          <ul className={classes.NavList}>
-            <li className={classes.NavListItem}>Home</li>
-            <li className={classes.NavListItem}>Shop</li>
-            <li className={classes.NavListItem}>About</li>
-            <li className={classes.NavListItem}>Contact</li>
-          </ul>
-        )}
-        {hamburgerIcon}
+        <CSSTransition
+          in={isNavOpen || screenWidth > 640}
+          nodeRef={nodeRef}
+          timeout={300}
+          classNames={{
+            enter: classes.slideEnter,
+            enterActive: classes.slideEnterActive,
+            exitActive: classes.slideExitActive,
+          }}
+          unmountOnExit
+        >
+          {
+            <ul className={classes.navLinks} ref={nodeRef}>
+              <NavLinks />
+            </ul>
+          }
+        </CSSTransition>
+        <HamburgerIcon onToggleNav={toggleNavHandler} />
       </nav>
     </header>
   );

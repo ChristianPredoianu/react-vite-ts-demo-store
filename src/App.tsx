cafd3164607b12fce31979e12ffee10b6156a87f
Christian Predoianu <christianpredoianu@yahoo.com>
2022-04-28 13:32:06 +0200

    added routes + latyout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from '@/pages/Home';
+import Layout from '@/components/layout/Layout';
+import '@/sass/main.scss';
 
-function App() {
-  return;
+function App(): JSX.Element {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;

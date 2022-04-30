2bc762cd9e0665534929c58db8ab8677bf048b14
Christian Predoianu <christianpredoianu@yahoo.com>
2022-04-30 09:44:40 +0200

    moved ul here

--- src/components/nav/NavLinks.tsx
+++ src/components/nav/NavLinks.tsx
@@ -1,12 +1,16 @@
 import classes from '@/components/nav/NavLinks.module.scss';
 
-export default function NavLinks() {
+interface navLinksProps {
+  ref: React.Ref<HTMLUListElement>;
+}
+
+export default function NavLinks({ ref }: navLinksProps) {
   return (
-    <>
+    <ul className={classes.navLinks} ref={ref}>
       <li className={classes.navLink}>Home</li>
       <li className={classes.navLink}>Shop</li>
       <li className={classes.navLink}>About</li>
       <li className={classes.navLink}>Contact</li>
-    </>
+    </ul>
   );
 }

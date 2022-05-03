4417e7b7feaa6ed2731f72ae8bbe5e7bbf58cc97
Christian Predoianu <christianpredoianu@yahoo.com>
2022-05-03 15:37:29 +0200

    added route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from '@/pages/Home';
 import Layout from '@/components/layout/Layout';
+import Home from '@/pages/Home';
+import Shop from '@/pages/Shop';
 import '@/sass/main.scss';
 
 function App(): JSX.Element {
@@ -9,6 +10,7 @@ function App(): JSX.Element {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="shop" element={<Shop />} />
         </Route>
       </Routes>
     </>

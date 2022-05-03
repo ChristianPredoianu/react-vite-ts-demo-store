7ed3d9c2217754eb2a370c3376671e9bd63ee91e
Christian Predoianu <christianpredoianu@yahoo.com>
2022-05-03 15:39:51 +0200

    added active class

--- src/components/nav/NavLinks.tsx
+++ src/components/nav/NavLinks.tsx
@@ -1,13 +1,21 @@
 import { forwardRef } from 'react';
+import { NavLink } from 'react-router-dom';
 import classes from '@/components/nav/NavLinks.module.scss';
 
 export default forwardRef<HTMLUListElement>(function NavLinks(props, ref) {
   return (
-    <ul className={classes.navLinks} ref={ref}>
-      <li className={classes.navLink}>Home</li>
-      <li className={classes.navLink}>Shop</li>
-      <li className={classes.navLink}>About</li>
-      <li className={classes.navLink}>Contact</li>
-    </ul>
+    <>
+      <ul className={classes.navLinks} ref={ref}>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? classes.activeLink : '')}
+        >
+          <li className={classes.navLink}>Home</li>
+        </NavLink>
+        <li className={classes.navLink}>Shop</li>
+        <li className={classes.navLink}>About</li>
+        <li className={classes.navLink}>Contact</li>
+      </ul>
+    </>
   );
 });

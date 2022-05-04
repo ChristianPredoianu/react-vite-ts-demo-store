43efab9e333047e7eb239149e11e9c6cb1d8c096
Christian Predoianu <christianpredoianu@yahoo.com>
2022-05-04 13:54:25 +0200

    added dynamic route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from '@/components/layout/Layout';
 import Home from '@/pages/Home';
 import Shop from '@/pages/Shop';
+import Category from '@/pages/Category';
 import '@/sass/main.scss';
 
 function App(): JSX.Element {
@@ -11,6 +12,7 @@ function App(): JSX.Element {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="shop" element={<Shop />} />
+          <Route path="shop/:category" element={<Category />} />
         </Route>
       </Routes>
     </>

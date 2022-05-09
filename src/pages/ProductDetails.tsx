6bfccc59f7b88b62ea9f932b82a88172627dcb17
Christian Predoianu <christianpredoianu@yahoo.com>
2022-05-09 14:26:46 +0200

    work on product details

---
+++ src/pages/ProductDetails.tsx
@@ -0,0 +1,66 @@
+import { useParams } from 'react-router-dom';
+import { useApi, apiResponse } from '@/hooks/useApi';
+import { useInputAmount } from '@/hooks/useInputAmount';
+import SocialMediaTab from '@/components/ui/SocialMediaTab';
+import LoadingSpinner from '@/components/ui/LoadingSpinner';
+import CtaBtn from '@/components/buttons/CtaBtn';
+import ProductInputAmount from '@/components/inputs/ProductInputAmount';
+import classes from '@/pages/ProductDetails.module.scss';
+import { JSXElementConstructor } from 'react';
+
+export default function ProductDetails() {
+  const { id } = useParams();
+
+  const { data, error, isLoading }: apiResponse = useApi(
+    `https://fakestoreapi.com/products/${id}`
+  );
+
+  const {
+    productAmount,
+    handleChange,
+    increaseCountHandler,
+    decreaseCountHandler,
+  } = useInputAmount();
+
+  const { title, price, description, image } = data;
+
+  let output;
+
+  if (isLoading) {
+    output = (
+      <div className={classes.loadingSpinner}>
+        <LoadingSpinner />
+      </div>
+    );
+  } else {
+    output = (
+      <>
+        <div className={classes.social}>
+          <SocialMediaTab />
+        </div>
+        <div className={classes.imgContainer}>
+          <img src={image} alt="product" className={classes.img} />
+        </div>
+        <div className={classes.productInfo}>
+          <h1 className={classes.title}>{title}</h1>
+          <p className={classes.description}>{description}</p>
+          <div className={classes.cta}>
+            <CtaBtn color={'green'}>Add to cart</CtaBtn>
+            <ProductInputAmount
+              productAmount={productAmount}
+              onInputChange={handleChange}
+              onDecreaseCount={decreaseCountHandler}
+              onIncreaseCount={increaseCountHandler}
+            />
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <div className="container">
+      <main className={classes.main}>{output}</main>
+    </div>
+  );
+}

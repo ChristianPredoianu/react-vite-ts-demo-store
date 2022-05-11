ec06e33c99492ef8fe60c221e2c67c91fbb9553c
Christian Predoianu <christianpredoianu@yahoo.com>
2022-05-11 14:07:21 +0200

    added cartItem component

---
+++ src/components/cart/CartItem.tsx
@@ -0,0 +1,44 @@
+import { ChangeEvent } from 'react';
+import ProductInputAmount from '@/components/inputs/ProductInputAmount';
+import classes from '@/components/cart/CartItem.module.scss';
+
+interface CartItemProps {
+  item: {
+    id: number;
+    title: string;
+    price: number;
+    image: string;
+    amount: number;
+  };
+  productAmount: number;
+  handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  increaseCountHandler: () => void;
+  decreaseCountHandler: () => void;
+}
+
+export default function CartItem({
+  item,
+  productAmount,
+  handleChange,
+  increaseCountHandler,
+  decreaseCountHandler,
+}: CartItemProps) {
+  const { id, title, price, image, amount } = item;
+
+  return (
+    <div className={classes.cartItem}>
+      <img src={image} alt="product" className={classes.img} />
+
+      <p className={classes.productTitle}></p>
+      <div className={classes.productAmount}>
+        <ProductInputAmount
+          productAmount={productAmount}
+          onInputChange={handleChange}
+          onDecreaseCount={decreaseCountHandler}
+          onIncreaseCount={increaseCountHandler}
+        />
+      </div>
+      <p className={classes.productPrice}>{price}</p>
+    </div>
+  );
+}

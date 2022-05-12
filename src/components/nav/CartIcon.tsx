954511ec07b405d933c67a14eece77720353d9fd
Christian Predoianu <christianpredoianu@yahoo.com>
2022-05-12 14:06:15 +0200

    displaying items in cart

--- src/components/nav/CartIcon.tsx
+++ src/components/nav/CartIcon.tsx
@@ -1,3 +1,5 @@
+import { useContext } from 'react';
+import cartContext from '@/store/cart-context/cartContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import classes from '@/components/nav/CartIcon.module.scss';
@@ -7,10 +9,17 @@ interface cartIconProps {
 }
 
 export default function CartIcon({ onOpenCart }: cartIconProps) {
+  const cartCtx = useContext(cartContext);
+  const { cartItems } = cartCtx;
+
+  const cartItemsCount = cartItems.reduce((currNum, item) => {
+    return currNum + item.amount;
+  }, 0);
+
   return (
     <div className={classes.cart}>
       <FontAwesomeIcon icon={faCartShopping} onClick={onOpenCart} />
-      <div className={classes.cartCount}>0</div>
+      <div className={classes.cartCount}>{cartItemsCount}</div>
     </div>
   );
 }

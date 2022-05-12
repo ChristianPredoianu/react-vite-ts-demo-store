7a119a99ade165f27bdc35148e3837821b7e1a11
Christian Predoianu <christianpredoianu@yahoo.com>
2022-05-12 14:04:13 +0200

    added interface

---
+++ src/types/apiData.interface.ts
@@ -0,0 +1,12 @@
+export interface ApiResponse {
+  category: string;
+  description: string;
+  id: number;
+  image: string;
+  price: number;
+  rating: {
+    rate: number;
+    count: number;
+  };
+  title: string;
+}

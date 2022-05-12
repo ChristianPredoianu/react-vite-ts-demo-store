07b4374e9759b65a82a29d3c030c6eacb5808540
Christian Predoianu <christianpredoianu@yahoo.com>
2022-05-12 14:06:30 +0200

    added type

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,14 +1,15 @@
 import { useState, useEffect } from 'react';
+import { ApiResponse } from '@/types/apiData.interface';
 
 export interface apiResponse {
-  data: any;
-  error: any;
+  data: Array<ApiResponse>;
+  error: string;
   isLoading: boolean;
 }
 
 export function useApi(url: string): apiResponse {
-  const [data, setData] = useState<[]>([]);
-  const [error, setError] = useState<any>();
+  const [data, setData] = useState([]);
+  const [error, setError] = useState<any>('');
   const [isLoading, setIsLoading] = useState(false);
 
   const fetchData = async () => {

43b8e400f8264b190e6e8c561bb8dfd0d4386039
Christian Predoianu <christianpredoianu@yahoo.com>
2022-05-12 14:06:51 +0200

    changed to add to cart

--- src/hooks/useInputAmount.ts
+++ src/hooks/useInputAmount.ts
@@ -1,20 +1,28 @@
-import { useState, ChangeEvent } from 'react';
+import { useState, ChangeEvent, useContext } from 'react';
+import cartContext from '@/store/cart-context/cartContext';
+import { ProductItem } from '@/types/productItem.interface';
 
-export function useInputAmount() {
-  const [productAmount, setProductAmount] = useState(1);
+export function useInputAmount(item: ProductItem) {
+  const [productAmount, setProductAmount] = useState(item.amount);
+
+  const cartCtx = useContext(cartContext);
+  const { addToCart, removeFromCart } = cartCtx;
 
   function handleChange(e: ChangeEvent<HTMLInputElement>) {
     if (e.target.value === '') setProductAmount(1);
     setProductAmount(+e.target.value);
   }
 
-  function increaseCountHandler() {
+  function increaseCountHandler(item: ProductItem) {
     setProductAmount((prevState) => (prevState += 1));
+    addToCart({ ...item, amount: 1 });
   }
 
-  function decreaseCountHandler() {
+  function decreaseCountHandler(item: ProductItem) {
     if (productAmount >= 2) setProductAmount((prevState) => (prevState -= 1));
+    if (productAmount > 0) removeFromCart(item.id);
   }
+
   return {
     productAmount,
     handleChange,

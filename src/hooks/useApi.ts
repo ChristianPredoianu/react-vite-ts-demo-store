617d5c96aad04b0e2ff0ad8daff1b65c771d7f8c
Christian Predoianu <christianpredoianu@yahoo.com>
2022-05-16 11:38:49 +0200

    final

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,12 +1,4 @@
 import { useState, useEffect } from 'react';
-import { ApiResponse } from '@/types/apiData.interface';
-
-/* export interface apiResponse<T> {
-  data?: T;
-  error?: string;
-  isLoading: boolean;
-}
- */
 
 const useApi = <T>(url: string) => {
   const [data, setData] = useState<T | null>(null);

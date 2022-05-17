8074b9999b46103963271936ad072c3ab94026c0
Christian Predoianu <christianpredoianu@yahoo.com>
2022-05-17 09:45:43 +0200

    added NavLinks + close nav when clicking on link

--- src/components/nav/NavLinks.tsx
+++ src/components/nav/NavLinks.tsx
@@ -2,19 +2,53 @@ import { forwardRef } from 'react';
 import { NavLink } from 'react-router-dom';
 import classes from '@/components/nav/NavLinks.module.scss';
 
-export default forwardRef<HTMLUListElement>(function NavLinks(props, ref) {
+interface NavLinksProps {
+  onCloseNav: () => void;
+}
+
+export default forwardRef<HTMLUListElement, NavLinksProps>(function NavLinks(
+  { onCloseNav },
+  ref
+) {
   return (
     <>
       <ul className={classes.navLinks} ref={ref}>
         <NavLink
           to="/"
-          className={({ isActive }) => (isActive ? classes.activeLink : '')}
+          className={({ isActive }) =>
+            isActive ? classes.activeLink : classes.inactiveLink
+          }
+          onClick={onCloseNav}
         >
           <li className={classes.navLink}>Home</li>
         </NavLink>
-        <li className={classes.navLink}>Shop</li>
-        <li className={classes.navLink}>About</li>
-        <li className={classes.navLink}>Contact</li>
+        <NavLink
+          to="/shop"
+          className={({ isActive }) =>
+            isActive ? classes.activeLink : classes.inactiveLink
+          }
+          onClick={onCloseNav}
+        >
+          <li className={classes.navLink}>Shop</li>
+        </NavLink>
+        <NavLink
+          to="about"
+          className={({ isActive }) =>
+            isActive ? classes.activeLink : classes.inactiveLink
+          }
+          onClick={onCloseNav}
+        >
+          <li className={classes.navLink}>About</li>
+        </NavLink>
+        <NavLink
+          to="contact"
+          className={({ isActive }) =>
+            isActive ? classes.activeLink : classes.inactiveLink
+          }
+          onClick={onCloseNav}
+        >
+          <li className={classes.navLink}>Contact</li>
+        </NavLink>
       </ul>
     </>
   );

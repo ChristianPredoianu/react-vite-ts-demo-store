ead3c32b0033519da95344b82b3a1c257ed6e2eb
Christian Predoianu <christianpredoianu@yahoo.com>
2022-05-18 11:07:07 +0200

    different styles for different buttons

--- src/components/buttons/CtaBtn.tsx
+++ src/components/buttons/CtaBtn.tsx
@@ -2,17 +2,25 @@ import classes from './CtaBtn.module.scss';
 import classNames from 'classnames';
 
 interface CtaBtnProps {
+  type: string;
   color: string;
   children: string;
   handleClick: () => void;
 }
 
-export default function CtaBtn({ children, color, handleClick }: CtaBtnProps) {
+export default function CtaBtn({
+  children,
+  color,
+  type,
+  handleClick,
+}: CtaBtnProps) {
   return (
     <button
       className={classNames(classes.ctaBtn, {
         [classes.blue]: color === 'blue',
         [classes.green]: color === 'green',
+        [classes.heroCta]: type === 'hero-cta',
+        [classes.cardBtn]: type === 'card-btn',
       })}
       onClick={handleClick}
     >

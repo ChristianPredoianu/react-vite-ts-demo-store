2c7e3f474ba267ba39c98904a88118944ba32eca
Christian Predoianu <christianpredoianu@yahoo.com>
2022-05-24 13:28:42 +0200

    added contact page + styles

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,23 @@
+import ContactForm from '@/components/forms/ContactForm';
+import ContactCard from '@/components/cards/ContactCard';
+import classes from '@/pages/Contact.module.scss';
+
+export default function Contact() {
+  return (
+    <div className="container">
+      <h1 className={classes.primaryHeading}>
+        Say <span className={classes.headingSpan}>Hello</span>
+      </h1>
+      <div className={classes.mainContent}>
+        <main className={classes.main}>
+          <ContactForm />
+          <div className={classes.contactInfo}>
+            <ContactCard content="(913) 125154" />
+            <ContactCard content="shopdrip@shopdrip.com" />
+            <ContactCard content="123 Woodstreet Palms, Los Angeles" />
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}

3b25ca8f3fc4e7ea49d11f9f61619a3025a5f6b9
Christian Predoianu <christianpredoianu@yahoo.com>
2022-05-30 11:12:01 +0200

    added scrollToTop component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,13 @@
 import Nav from '@/components/nav/Nav';
 import { Outlet } from 'react-router-dom';
+import ScrollToTop from '@/components/ui/ScrollToTop';
 
 export default function Layout() {
   return (
     <>
       <Nav />
       <Outlet />
+      <ScrollToTop />
     </>
   );
 }

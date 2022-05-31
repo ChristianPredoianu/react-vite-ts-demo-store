4dd2128793bd3d92127747b0e47f855c134d824a
Christian Predoianu <christianpredoianu@yahoo.com>
2022-05-31 13:58:05 +0200

    changed inteface name

--- src/components/nav/SearchIcon.tsx
+++ src/components/nav/SearchIcon.tsx
@@ -2,11 +2,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import classes from '@/components/nav/SearchIcon.module.scss';
 
-interface searchProps {
+interface SearchIconProps {
   onOpenSearch: () => void;
 }
 
-export default function SearchIcon({ onOpenSearch }: searchProps) {
+export default function SearchIcon({ onOpenSearch }: SearchIconProps) {
   return (
     <div className={classes.searchIcon}>
       <FontAwesomeIcon icon={faSearch} onClick={onOpenSearch} />

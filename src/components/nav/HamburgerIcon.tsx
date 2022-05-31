79ff2eeb6a9b4c0e8d765de2fd1707c49e3d9ed0
Christian Predoianu <christianpredoianu@yahoo.com>
2022-05-31 13:49:19 +0200

    changed inteface name

--- src/components/nav/HamburgerIcon.tsx
+++ src/components/nav/HamburgerIcon.tsx
@@ -2,11 +2,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import classes from '@/components/nav/HamburgerIcon.module.scss';
 
-interface hamburgerProps {
+interface HamburgerIconProps {
   onToggleNav: () => void;
 }
 
-export default function HamburgerIcon({ onToggleNav }: hamburgerProps) {
+export default function HamburgerIcon({ onToggleNav }: HamburgerIconProps) {
   return (
     <div className={classes.hamburger}>
       <FontAwesomeIcon icon={faBars} onClick={onToggleNav} />

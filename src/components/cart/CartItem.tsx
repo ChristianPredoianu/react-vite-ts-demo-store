b1590c4e3ed808d3c0131eb6abd07033447dee4e
Christian Predoianu <christianpredoianu@yahoo.com>
2022-05-31 09:27:24 +0200

    made some styling tweaks

--- src/components/cart/CartItem.tsx
+++ src/components/cart/CartItem.tsx
@@ -18,18 +18,21 @@ export default function CartItem({ item }: CartItemProps) {
   } = useInputAmount(item);
 
   return (
-    <div className={classes.cartItem}>
-      <img src={image} alt="product" className={classes.img} />
-      <p className={classes.productTitle}></p>
-      <div className={classes.productAmount}>
-        <ProductInputAmount
-          productAmount={productAmount}
-          onInputChange={handleChange}
-          onDecreaseCount={() => decreaseCountHandler(item)}
-          onIncreaseCount={() => increaseCountHandler(item)}
-        />
+    <>
+      <div className={classes.cartItem}>
+        <img src={image} alt="product" className={classes.img} />
+        <p className={classes.productTitle}></p>
+        <div className={classes.productAmount}>
+          <ProductInputAmount
+            productAmount={productAmount}
+            onInputChange={handleChange}
+            onDecreaseCount={() => decreaseCountHandler(item)}
+            onIncreaseCount={() => increaseCountHandler(item)}
+          />
+        </div>
+        <p className={classes.productPrice}>{`${price} $ x ${amount}`}</p>
       </div>
-      <p className={classes.productPrice}>{price}</p>
-    </div>
+      <p className={classes.productTitle}>{title}</p>
+    </>
   );
 }

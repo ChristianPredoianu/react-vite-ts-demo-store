4ea918b4cc5166b55d477b72be440a79ccd7b1ce
Christian Predoianu <christianpredoianu@yahoo.com>
2022-05-31 12:59:28 +0200

    removed footer from layout component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,19 +1,13 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Nav from '@/components/nav/Nav';
 import ScrollToTop from '@/components/ui/ScrollToTop';
-import Footer from '@/components/layout/footer/Footer';
 
 export default function Layout() {
-  const location = useLocation();
-
-  console.log(location.pathname);
-
   return (
     <>
       <Nav />
       <Outlet />
       <ScrollToTop />
-      {location.pathname !== '/' && <Footer />}
     </>
   );
 }

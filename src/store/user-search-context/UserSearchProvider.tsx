8489f7ddb69ca0ae0ac1fc3d120aa4fbe9f87292
Christian Predoianu <christianpredoianu@yahoo.com>
2022-05-31 14:09:44 +0200

    removed useEffect import

--- src/store/user-search-context/UserSearchProvider.tsx
+++ src/store/user-search-context/UserSearchProvider.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import UserSearchContext from '@/store/user-search-context/userSearchContext';
 
 interface UserSearchProps {

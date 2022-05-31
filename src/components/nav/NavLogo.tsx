11f1d006c4742c063724611e03685136489ca769
Christian Predoianu <christianpredoianu@yahoo.com>
2022-05-31 10:38:25 +0200

    added link to '/'

--- src/components/nav/NavLogo.tsx
+++ src/components/nav/NavLogo.tsx
@@ -1,6 +1,11 @@
+import { Link } from 'react-router-dom';
 import Logo from '@/assets/logo.png';
 import classes from '@/components/nav/NavLogo.module.scss';
 
 export default function NavLogo() {
-  return <img src={Logo} alt="Logo" className={classes.logo} />;
+  return (
+    <Link to={'/'}>
+      <img src={Logo} alt="Logo" className={classes.logo} />
+    </Link>
+  );
 }
